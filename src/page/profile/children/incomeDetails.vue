948081e880a6ee22c934c0d5ac7d4a01d2e33eae
yuyang <yuyang@hoolaicom>
2018-11-30 15:36:02 +0800

    feat(incomeDetail):收益明细页面布局

---
+++ src/page/profile/children/incomeDetails.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="income_details header-top">
+    <head-top head-title="收益明细" go-back='true'></head-top>
+    <ul>
+      <li v-for="(item,index) in detailList" :key="index">
+        <p>{{item.date | date('.')}}</p>
+        <p class="ellipsis">{{item.desr}}</p>
+        <p>{{item.money > 0 ? '+' : ''}}{{item.money}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  import headTop from '../../../components/header/head'
+  export default {
+    data () {
+      return {
+        detailList: [
+          {
+            date: 16732211111,
+            desr: '某某某消费230元某某某消费230元',
+            money: 1.25
+          },
+          {
+            date: 11555322222,
+            desr: '提现20元',
+            money: -25
+          }
+        ]
+      }
+    },
+    created () {
+    },
+    components: {
+      headTop
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  @import "src/style/mixin";
+  .income_details {
+    ul {
+      overflow: hidden;
+      background-color: $fc;
+      width: 100%;
+      clear: both;
+      li {
+        @include wh(100%, .55rem);
+        border-bottom: .01rem solid $bc;
+        line-height: .55rem;
+        padding: 0 .12rem;
+        clear: both;
+        overflow: hidden;
+        // box-sizing: content-box;
+        p {
+          float: left;
+          @include sc(.15rem, $g6);
+          width: 30%;
+        }
+        p:nth-child(2) {
+          width: 50%;
+        }
+        p:last-child {
+          color: $red;
+          text-align: right;
+          width: 20%;
+        }
+      }
+    }
+  }
+</style>
