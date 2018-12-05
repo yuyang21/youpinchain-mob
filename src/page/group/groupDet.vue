9e7df5f24981bb6c25243beb74c1541433ef52ce
zhangfeng <zf798904060@163.com>
2018-12-05 09:45:29 +0800

    fix(拼团详情)：逻辑错误，groupMy 未定义

--- src/page/group/groupDet.vue
+++ src/page/group/groupDet.vue
@@ -122,6 +122,7 @@
                 headTitle: "",
                 goodsid: "",
                 groupMyId: '',
+                groupMy: {},
                 groupSuit: {},
                 suitTypes: [],
                 suitDet: [],
