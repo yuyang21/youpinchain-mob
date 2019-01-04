ecc5b27bf0829daa5002f5a7ad30e14df33e9ff3
yu yang <18443225359@163.com>
2019-01-04 17:40:30 +0800

    fix(order):解决多个下拉刷新问题

--- src/page/order/children/completed.vue
+++ src/page/order/children/completed.vue
@@ -1,57 +1,59 @@
 <template>
-    <div class="unpaid">
-        <ul class="order_list_ul">
-            <div v-if="orderList.length <= 0" class="no_list">暂无订单记录</div>
-            <li v-else class="order_list_li" v-for="item in orderList" :key="item.id">
-                <section class="order_item_top">
-                    <section>
-                        <header class="order_item_top_header">
-                            <p class="order_time">{{item.addTime | timeformat}}</p>
-                            <p class="order_status">
-                                <template v-if="item.groupMyType == 2">(拼团成功)</template>
-                                <template v-if="item.groupMyType == 3">(拼团失败)</template>
-                                {{item.orderStatusText}}
-                            </p>
-                        </header>
-                        <router-link :to="{path:'/orderDetail/' + item.id}" tag="div">
-                            <section class="goods_img">
-                                <div class="goods_box">
-                                    <img class="restaurant_image" v-for="(goods,index) in item.productList"
-                                         :src="goods.pictureUrl" v-if="index < 4" :key="index">
-                                    <!-- <p class="" v-if="item.productList.length == 1">{{item.productList[0].productName}}</p> -->
-                                </div>
-                                <p>共{{item.productList.length}}件</p>
-                                <svg fill="#bbb" style="width: 0.15rem;margin-left: 0rem;height: .64rem;">
-                                    <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
-                                </svg>
-                            </section>
-                        </router-link>
-                    </section>
-                    <div class="order_item_bottom">
-                        <span class="order_text">实际支付<b class="red"><span class="RMB">￥</span><strong
-                                class="order_price_size">{{item.actualPrice}}</strong></b></span>
-                        <div class="order_button_border_grey" @click="showAlertTip = !showAlertTip">联系客服</div>
-                        <div class="order_again">
-                            <compute-time v-if="item.handleOption.pay" :time="item.expiryTime"></compute-time>
-                            <!--<span class="order_button_border_red" @click="cancelOrder(item.id)" v-if="item.handleOption.cancel">取消订单</span>-->
-                            <router-link :to="{path:'/orderTrack/'+item.expNo + '/' + item.expCode}" tag="span"
-                                         class="order_button_border_red"
-                                         v-if="item.handleOption.confirm">查看物流
+    <div class="mescroll" ref="mescroll">
+        <div class="unpaid">
+            <ul class="order_list_ul">
+                <div v-if="orderList.length <= 0" class="no_list">暂无订单记录</div>
+                <li v-else class="order_list_li" v-for="item in orderList" :key="item.id">
+                    <section class="order_item_top">
+                        <section>
+                            <header class="order_item_top_header">
+                                <p class="order_time">{{item.addTime | timeformat}}</p>
+                                <p class="order_status">
+                                    <template v-if="item.groupMyType == 2">(拼团成功)</template>
+                                    <template v-if="item.groupMyType == 3">(拼团失败)</template>
+                                    {{item.orderStatusText}}
+                                </p>
+                            </header>
+                            <router-link :to="{path:'/orderDetail/' + item.id}" tag="div">
+                                <section class="goods_img">
+                                    <div class="goods_box">
+                                        <img class="restaurant_image" v-for="(goods,index) in item.productList"
+                                            :src="goods.pictureUrl" v-if="index < 4" :key="index">
+                                        <!-- <p class="" v-if="item.productList.length == 1">{{item.productList[0].productName}}</p> -->
+                                    </div>
+                                    <p>共{{item.productList.length}}件</p>
+                                    <svg fill="#bbb" style="width: 0.15rem;margin-left: 0rem;height: .64rem;">
+                                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#arrow-right"></use>
+                                    </svg>
+                                </section>
                             </router-link>
-                            <span class="order_button_border_red" @click="confirmOrder(item.id)"
-                                  v-if="item.handleOption.confirm">确认收货</span>
-                            <span class="order_button_border_red" @click="rebuy(item.id)"
-                                  v-if="item.handleOption.rebuy">再次购买</span>
+                        </section>
+                        <div class="order_item_bottom">
+                            <span class="order_text">实际支付<b class="red"><span class="RMB">￥</span><strong
+                                    class="order_price_size">{{item.actualPrice}}</strong></b></span>
+                            <div class="order_button_border_grey" @click="showAlertTip = !showAlertTip">联系客服</div>
+                            <div class="order_again">
+                                <compute-time v-if="item.handleOption.pay" :time="item.expiryTime"></compute-time>
+                                <!--<span class="order_button_border_red" @click="cancelOrder(item.id)" v-if="item.handleOption.cancel">取消订单</span>-->
+                                <router-link :to="{path:'/orderTrack/'+item.expNo + '/' + item.expCode}" tag="span"
+                                            class="order_button_border_red"
+                                            v-if="item.handleOption.confirm">查看物流
+                                </router-link>
+                                <span class="order_button_border_red" @click="confirmOrder(item.id)"
+                                    v-if="item.handleOption.confirm">确认收货</span>
+                                <span class="order_button_border_red" @click="rebuy(item.id)"
+                                    v-if="item.handleOption.rebuy">再次购买</span>
+                            </div>
                         </div>
-                    </div>
-                </section>
-            </li>
-        </ul>
-        <alert-tip :showAlertTip="showAlertTip" :type="2" v-show="showAlertTip"
-                   :alertText="`<p>请添加客服微信 <br> Lsyp-123</p>`"></alert-tip>
-        <transition name="loading">
-            <loading v-show="showLoading"></loading>
-        </transition>
+                    </section>
+                </li>
+            </ul>
+            <alert-tip :showAlertTip="showAlertTip" :type="2" v-show="showAlertTip"
+                    :alertText="`<p>请添加客服微信 <br> Lsyp-123</p>`"></alert-tip>
+            <transition name="loading">
+                <loading v-show="showLoading"></loading>
+            </transition>
+        </div>
     </div>
 </template>
 
