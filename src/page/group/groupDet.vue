9575c74c55b603eb5b0e63f13ae81e16d4d03033
yu yang <yuyang1@hoolai.com>
2019-01-09 18:53:09 +0800

    feat(groupDet):详情页添加视频vue-video-player插件

--- src/page/group/groupDet.vue
+++ src/page/group/groupDet.vue
@@ -8,10 +8,12 @@
                 <div v-if="headPic && headPic.length <= 0">
                     <img :src="groupSuit.normalPic" alt="" class="show">
                 </div>
-                <swipe ref="swipe" :speed="500" :loop="true" :autoplayTime="1500" v-else>
+                <swipe ref="swipe" :speed="500" :loop="true" :autoplayTime="0" v-else>
                     <swipe-item v-for="(item,index) in headPic" :key="index">
                         <div class="header_image">
-                            <img :src="item" alt="" width="100%" class="show">
+                            <video-player class="video-player vjs-custom-skin show" ref="videoPlayer" :playsinline="true" :options="playerOptions" v-show="item.indexOf('mp4') != -1"></video-player>
+                            <!-- <video :src="item" id="videoPlay" v-show="item.indexOf('mp4') != -1" class="show" @click="playVideo()"></video> -->
+                            <img :src="item" v-show="item.indexOf('mp4') == -1" width="100%" class="show">
                         </div>
                     </swipe-item>
                 </swipe>
@@ -174,7 +176,30 @@
                 timer: null,
                 headPic: [],
                 footPic: [],
-                brandId: null
+                brandId: null,
+                playerOptions : {
+                    playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+                    autoplay: false, //如果true,浏览器准备好时开始回放。
+                    muted: false, // 默认情况下将会消除任何音频。
+                    loop: false, // 导致视频一结束就重新开始。
+                    preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+                    language: 'zh-CN',
+                    // aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+                    fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+                    sources: [{
+                        type: "",
+                        src: "" //url地址
+                    }],
+                    // poster: "../../static/images/test.jpg", //你的封面地址
+                    // width: document.documentElement.clientWidth,
+                    notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+                    controlBar: {
+                        timeDivider: false,
+                        durationDisplay: false,
+                        remainingTimeDisplay: false,
+                        fullscreenToggle: false  //全屏按钮
+                    }
+                }
             };
         },
         watch: {
@@ -204,6 +229,10 @@
         computed: {},
         props: ["showErrMsg"],
         methods: {
+            playVideo () {
+                var vdo = document.getElementById("videoPlay");
+                vdo.play();
+            },
             initData() {
                 var that = this;
                 groupSuit(that.groupSuitId).then(res => {
@@ -215,6 +244,13 @@
                     that.suitTypes = res.data.suitTypes;
                     that.headPic = res.data.headPic;
                     that.footPic = res.data.footPic;
+
+                    that.headPic.forEach(img => {
+                        if (img.indexOf('mp4') != -1) {
+                            console.log(img)
+                            that.playerOptions.sources[0].src = img;
+                        }
+                    })
 
                     // that.startTimeDown = res.data.startTimeDown;
                     wx.ready(function () {
